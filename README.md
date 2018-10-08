63508ed11603d5fe6800f47d3b8865c55afc62a3
strawp <github@strawp.net>
2018-10-08 13:05:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ocatodo
+
 Download your last Ocado shopping list as a todo.txt list
+
+## Why
+
+Ocado stopped giving you a printout of your shopping list to tick off items from, which sucked. This script logs into your account and downloads your last shopping list, outputting it in `todo.txt` format.
+
+## Usage
+
+```
+python ocatodo.py -u mysername@example.com -p mypassw0rd >> ~/Dropbox/todo/todo.txt
+```
+
+## Dependencies
+
+`argparse`, `requests`
+
+
---
+++ ocatodo.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+from lxml import etree
+import argparse, requests, sys
+
+class OcadoClient:
+
+  userid    = None
+  sessionid = '8BFF9C71-CA91-4BA5-BE99-605806AEE64F'
+  deviceid  = '8d8c124b51fe43eb'
+  authtoken = '861e3a4f4da0819608f4c928da0274f6'
+  useragent = 'Ocado-Android-Application/1.55.11'
+  host      = 'mobile.ocado.com'
+
+  def __init__( self, username, password ):
+    self.login( username, password )
+
+  def do_request( self, path, method='GET', data=None, headers=None, responseformat='json' ):
+    url = 'https://' + self.host + path
+    hdrs = {
+      'User-Agent': self.useragent,
+      'sessionId': self.sessionid,
+      'Authorization': 'token:' + self.authtoken,
+      'Accept': 'application/json,*/*'
+    }
+    if headers: hdrs.update( headers )
+    if data: hdrs['Content-Length'] = str(len(str(data)))
+    response = requests.request( method, url, data=data, headers=hdrs )
+    
+    if not str( response.status_code ).startswith('2'):
+      print response.text
+      return False
+
+    if responseformat == 'json':
+      return response.json()
+
+    if responseformat == 'xml':
+      return etree.fromstring( response.text )
+
+  def login( self, username, password ):
+    rsp = self.do_request( '/webservices/mobileDevice/' + self.deviceid, 'POST', 
+      {
+        'username': username,
+        'password': password
+      }
+    )
+    if not rsp:
+      return False
+    self.authtoken = rsp['token']
+    self.userid    = rsp['customerNo']
+  
+  def get_last_orderid( self ):
+    rlt = self.do_request( '/webservices/user/'+str( self.userid )+'/orders/status', responseformat='xml' )
+    orders = rlt.xpath( '//order' )
+    for o in orders:
+      return o.attrib['id']
+    return False
+
+  def get_order( self, orderid ):
+    rlt = self.do_request( '/webservices/user/'+str( self.userid )+'/orders/order/' + str( orderid ) )
+    return rlt
+
+  def get_last_order_details( self ):
+    orderid = self.get_last_orderid()
+    return self.get_order( orderid )
+
+def main():
+  parser = argparse.ArgumentParser(description="Get your last Ocado shopping list in todo.txt format")
+  parser.add_argument("-u", "--username", help="Ocado username")
+  parser.add_argument("-p", "--password", help="Ocado password")
+  args = parser.parse_args()
+  if len( sys.argv)==1:
+    parser.print_help()
+    return
+  oc = OcadoClient(args.username,args.password)
+  order = oc.get_last_order_details()
+  if order:
+    for item in order['items']:
+      q = item['quantity']
+      print item['desc'] + ' (' + q['delivered'] + '/' +q['ordered']+ ')' + ' @ocado +' + item['category']
+
+if __name__ == "__main__":
+  main()
+
